d29acdc01b0f93406a412614e4879ab5461a7ab4
Ryan Allen <rrallen2002@gmail.com>
2020-11-27 16:39:42 -0500

    Made previous terminal lines uneditable & added command capture

--- MacroBuddy_src/src/Terminal.java
+++ MacroBuddy_src/src/Terminal.java
@@ -1,13 +1,16 @@
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
-public class Terminal {
+public class Terminal{
 
     JTextArea terminalWindow;
+    ArrayList<String> current_commands = new ArrayList<String>();
+    String current_command;
 
     public Terminal() {
 
@@ -15,7 +18,44 @@ public class Terminal {
         terminalWindow.setColumns(20);
         terminalWindow.setForeground(Color.white);
         terminalWindow.setBackground(Color.black);
+        terminalWindow.setCaretColor(Color.WHITE);
+
+        terminalWindow.addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e){
+
+                int row = 0;
+                try {
+                    row = terminalWindow.getLineOfOffset(terminalWindow.getCaretPosition());
+                } catch (BadLocationException e1) {
+                    e1.printStackTrace();
+                }
+                if (row + 1 < terminalWindow.getLineCount()) {
+                    terminalWindow.setCaretPosition(terminalWindow.getDocument().getLength());
+                }
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
 
     }
 
+    public void capture_commands(){
+        for (String line : terminalWindow.getText().split("\\n")) {
+            current_commands.add(line);
+            System.out.println("HERE's A LINE: " + current_command);
+        }
+    }
+
+
+
 }
+
